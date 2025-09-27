a5e9adb3bc66120d3006fcf09ab938db77305edc
Ezz Al-din <doctorezz24@gmail.com>
2025-09-27 19:05:00 +0300

    2nd Setup

---
+++ copaw/lib/Feature/Ai/screens/ai_assistant_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AiAssistantScreen extends StatelessWidget {
+  const AiAssistantScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(backgroundColor: Colors.black));
+  }
+}
