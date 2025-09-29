171de7cdad674e9c5f8914dd05eb9ff50b5b655b
Omar Nassar <omarprnass@gmail.com>
2025-09-30 00:24:05 +0300

    Add custom_button.dart

---
+++ copaw/lib/Feature/widgets/common/custom_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Function? onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.label,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 420,
+      height: 55,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 24, 76, 165),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        onPressed: () => onPressed!(),
+        child: Text(
+          label,
+          style:  TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
