8a691509eedac8cd411bcd9ecc0093af2db4b2b7
Hassan-Mohamed <hassanmohamed5750@gmail.com>
2025-10-07 00:49:57 +0300

    handle custom text form feild

--- copaw/lib/Feature/widgets/common/custom_text_field.dart
+++ copaw/lib/Feature/widgets/common/custom_text_field.dart
@@ -1,24 +1,26 @@
 import 'package:copaw/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
-typedef onValidator = String? Function(String?)? ;
+typedef onValidator = String? Function(String?)?;
+
 class CustomTextFormField extends StatelessWidget {
   Color colorBorder;
-  String? hintText ;
+  String? hintText;
   TextStyle? hintStyle;
-  String? labelText ;
+  String? labelText;
   TextStyle? labelStyle;
   Widget? prefixIcon;
   Widget? suffixIcon;
   TextEditingController controller;
-  onValidator? validator ;
-  TextInputType? keyBoardType ;
-  bool obscureText ;
-  String? obscuringCharacter ;
+  onValidator? validator;
+  TextInputType? keyBoardType;
+  bool obscureText;
+  String? obscuringCharacter;
   int? maxLines;
 
 
-  CustomTextFormField({super.key,
+  CustomTextFormField({
+    super.key,
     this.colorBorder = AppColors.grayColor,
     this.hintText,
     this.hintStyle,
@@ -28,44 +30,59 @@ class CustomTextFormField extends StatelessWidget {
     this.suffixIcon,
     required this.controller,
     this.validator,
-    this.keyBoardType = TextInputType.text ,
-    this.obscureText = false ,
+    this.keyBoardType = TextInputType.text,
+    this.obscureText = false,
     this.obscuringCharacter,
-    this.maxLines
+    this.maxLines,
   });
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       maxLines: maxLines ?? 1,
       decoration: InputDecoration(
-
         enabledBorder: borderDecoration(colorBorder: colorBorder),
         focusedBorder: borderDecoration(colorBorder: colorBorder),
         errorBorder: borderDecoration(colorBorder: AppColors.warningColor),
-        focusedErrorBorder: borderDecoration(colorBorder: AppColors.warningColor),
+        focusedErrorBorder: borderDecoration(
+          colorBorder: AppColors.warningColor,
+        ),
         hintText: hintText,
-        hintStyle: hintStyle ?? TextStyle( color: AppColors.grayColor,fontSize: 16 , fontWeight: FontWeight.w500),
+        hintStyle:
+            hintStyle ??
+            TextStyle(
+              color: AppColors.grayColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
         labelText: labelText,
-        labelStyle: labelStyle ??TextStyle( color: AppColors.grayColor,fontSize: 16 , fontWeight: FontWeight.w500),
+        labelStyle:
+            labelStyle ??
+            TextStyle(
+              color: AppColors.grayColor,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
         prefixIcon: prefixIcon,
         suffix: suffixIcon,
-        errorStyle:TextStyle( color: AppColors.warningColor,fontSize: 16 , fontWeight: FontWeight.w500)
+        errorStyle: TextStyle(
+          color: AppColors.warningColor,
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
       ),
-      keyboardType: keyBoardType ,
-      obscureText: obscureText ,
-      controller: controller ,
+      keyboardType: keyBoardType,
+      obscureText: obscureText,
+      controller: controller,
       validator: validator,
       cursorColor: AppColors.mainColor,
-      obscuringCharacter: obscuringCharacter ?? '.' ,
+      obscuringCharacter: obscuringCharacter ?? '.',
     );
   }
-  OutlineInputBorder borderDecoration({required Color colorBorder}){
+
+  OutlineInputBorder borderDecoration({required Color colorBorder}) {
     return OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(
-            color: colorBorder ,
-            width: 2
-        )
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: colorBorder, width: 2),
     );
   }
 }
