bcf58c1af8bde16288a9c94be7eac426569bddff
Ezz Al-din <doctorezz24@gmail.com>
2025-10-08 00:36:08 +0300

    Adding Custom Container ,project Picker (Dummy data) AI SCREEN :)

--- copaw/lib/Feature/Ai/screens/ai_assistant_screen.dart
+++ copaw/lib/Feature/Ai/screens/ai_assistant_screen.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:copaw/Feature/widgets/common/appbar.dart';
+import 'package:copaw/utils/app_colors.dart';
+import 'package:copaw/Feature/widgets/AI/ProjectList.dart';
 
 class AiAssistantScreen extends StatelessWidget {
   AiAssistantScreen({super.key});
-MyCustomAppBar appBar1 = MyCustomAppBar(head: 'Title', img: null);  
+
+  final MyCustomAppBar appBar1 = MyCustomAppBar(
+    head: 'AI Assistant',
+    img: null,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: appBar1,
+      appBar: appBar1,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
 
+            const Projectlist(),
 
+       
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ copaw/lib/Feature/widgets/AI/CustomContainer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:copaw/utils/app_colors.dart';
+
+class Customcontainer extends StatelessWidget {
+  final double Width;
+  final double Height;
+  final Widget? child;
+
+  const Customcontainer({
+    required this.Height,
+    required this.Width,
+    this.child,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: AppColors.grayColor.withOpacity(0.3)),
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 6,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          width: Width,
+          height: Height,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
